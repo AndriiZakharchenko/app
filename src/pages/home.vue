f04052f452c6ea4df8cb28b2f5933d6fd52baf63
Andrey Zakharchenko <andrey.zakharchenko@eltexsoft.com>
2020-07-12 22:01:16 +0300

    fix filters on page & deploy

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,9 +11,9 @@
     </md-snackbar>
     <h1>Home</h1>
     <div class="filter-list">
-      <a href="#" ref="defaultFilter" @click="filterFilms('all')" :class="{active: currentFilter === 'all'}">All</a>
-      <a href="#" @click="filterFilms('completed')" :class="{active: currentFilter === 'completed'}">Completed</a>
-      <a href="#" @click="filterFilms('active')" :class="{active: currentFilter === 'active'}">Active</a>
+      <a href="#" ref="defaultFilter" @click="filterFilms('all', $event)">All</a>
+      <a href="#" @click="filterFilms('completed', $event)">Completed</a>
+      <a href="#" @click="filterFilms('active', $event)">Active</a>
     </div>
     <div class="film">
       <transition-group enter-active-class="animated fadeInUp" leave-active-class="animated fadeInDown">
@@ -37,8 +37,6 @@
 </template>
 
 <script>
-// import { mapState } from 'vuex';
-
 export default {
   name: 'Home',
   mounted() {
@@ -56,16 +54,12 @@ export default {
     message: '',
     showMessage: false,
     films: [],
-    currentFilter: '',
   }),
-  computed: {
-    // ...mapState({
-    //   films: state => state.films.films,
-    // }),
-  },
   methods: {
-    filterFilms(val) {
-      this.currentFilter = val;
+    filterFilms(val, event) {
+      const filterElements = document.querySelectorAll('.filter-list a');
+      filterElements.forEach(elem => elem.classList.remove('active'));
+      event.target.classList.add('active');
       return this.films = this.$store.getters['films/filterFilms'](val);
     }
   }
