752919538b6b52143605234239364030869e5b13
Andrey Zakharchenko <andrey.zakharchenko@eltexsoft.com>
2020-09-30 14:58:27 +0300

    final step

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <fieldset class="form-group" :class="{ 'input-error': hasErrors }">
+    <label v-if="label">{{ label }}</label>
+    <slot></slot>
+    <div class="errors" v-if="hasErrors">
+      <div
+        class="error"
+        v-for="error in activeErrorMessages"
+        :key="error"
+      >{{ error }}</div>
+    </div>
+  </fieldset>
+</template>
+
+<script>
+import { singleErrorExtractorMixin } from 'vuelidate-error-extractor';
+
+export default {
+  mixins: [singleErrorExtractorMixin],
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
